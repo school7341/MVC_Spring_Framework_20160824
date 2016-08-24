e4cda2ffcfaa96f6ac91bdd0b74962bf9ef75562
github school7341 <github school7341@naver.com>
2016-08-24 14:32:47 +0900

    1st : Tight-Coupling

---
+++ hello/HelloApp.java
@@ -0,0 +1,11 @@
+package yong.hello01;
+
+public class HelloApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MessageBean bean = new MessageBean();
+		bean.sayHello("Java");
+	}
+
+}
